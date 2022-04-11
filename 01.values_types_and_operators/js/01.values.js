b7d0178921dbb7ee1f6f49db33920dbba6db35f9
marcdc-tm <marc.decaluwe@thomasmore.be>
2022-04-11 19:45:22 +0200

    00.values.js met console.logs

--- 01.values_types_and_operators/js/01.values.js
+++ 01.values_types_and_operators/js/01.values.js
@@ -13,9 +13,9 @@ console.log(undefinedvariable)
 const berekening = 25987 * 342;
 console.log(berekening);
 
-console.log("Dit is een enkel aanhalingsteken: ‘\nEn dit een dubbel: \"");
+console.log("Dit is een enkel aanhalingsteken: '\nEn dit een dubbel: \"");
 console.log("deze string wordt geconcateneerd " + "met deze string");
-console.log(`dit is een template literal en bevat een expressie: ${5 * 22}`);
+console.log(`dit is een template literal en bevat een expressie: ${5 * 22} en de berekening is ${berekening}`);
 
 console.log("======================================");
 console.log("=== Operator typeof");
@@ -41,42 +41,46 @@ console.log("======================================");
 console.log("=== Speciale Numbers");
 //voer deze lijnen 1 voor 1 uit in de console:
 Infinity
-9 / 0
-- 9 / 0;
-(9 / 0) + 5
-9 / (0 + 5)
-0 / 0
-"aap" / 100
-NaN == NaN // NaN is aan niks gelijk !
+console.log(9 / 0);
+console.log(- 9 / 0);
+console.log((9 / 0) + 5);
+console.log(9 / (0 + 5));
+console.log(0 / 0);
+console.log("aap" / 100);
+console.log(NaN == NaN) // NaN is aan niks gelijk !
 
 
 console.log("======================================");
 console.log("=== Type conversies");
 //voer deze lijnen 1 voor 1 uit in de console:
-8 * null
-"5" - 1
-"5" + 1;
-"vijf" * 2;
-false == 0;
-false === 0
-null == undefined
-null == 0
-null || "user"
-"Agnes" || "user"
-2.5
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("vijf" * 2);
+console.log(false == 0);
+console.log(false === 0);
+console.log(null == undefined);
+console.log(null == 0);
+console.log(null || "user");
+console.log("Agnes" || "user");
+console.log(2.5);
 
 
 console.log("======================================");
 console.log("=== Equality operators");
 //voer deze lijnen 1 voor 1 uit in de console:
-5 == 6
-5 == 5
-true == 1
-true === 1
-null == 0
-0 == ""
-0 === ""
-5 == "5"
-5 === "5"
+console.log(5 == 6);
+console.log(5 == 5);
+console.log(true == 1);
+console.log(true === 1);
+console.log(null == 0);
+console.log(0 == "");
+console.log(0 === "");
+console.log(5 == "5");
+console.log(5 === "5");
+
+
+const bmi = 67 / Math.pow(1.8, 2);
+console.log(bmi);
 
 
