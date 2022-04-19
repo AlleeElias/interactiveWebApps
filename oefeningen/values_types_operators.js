8edcea77f2cdf6e39e23e29f5fdc2846bf159274
AlleeElias <eliasneel2001@gmail.com>
2022-04-19 11:57:43 +0200

    oef1

---
+++ oefeningen/values_types_operators.js
@@ -0,0 +1,78 @@
+console.log("hallo!");
+console.log(5+5);
+
+const text = "dit is een tekst";
+console.log(text);
+
+let undefinedvariable;
+console.log(undefinedvariable)
+
+const berekening = 25987 * 342;
+console.log(berekening);
+
+console.log(`deze template literal bevat een expressie: ${5 * 22} en de berekening is ${berekening}`);
+
+console.log("Dit is een enkel aanhalingsteken: '\nEn dit een dubbel: \"");
+
+console.log("deze string wordt geconcateneerd " + "met deze string");
+console.log(`typeof(\"x\") = ${typeof ("x")}`);
+
+console.log(`typeof(55) = ${typeof (55)}`);
+
+console.log(`typeof(true) = ${typeof (true)}`);
+
+const c = '44';
+console.log(`typeof(c) = ${typeof (c)}`);
+
+console.log(3 > 2);
+
+console.log(false ? 1 : 2);
+
+/*
+"use strict";
+
+a = 4;
+console.log(a);*/
+
+console.log(9 / 0);
+
+console.log(- 9 / 0);
+
+console.log((9 / 0) + 5);
+
+console.log(9 / (0 + 5));
+
+console.log(NaN == NaN);
+
+console.log(8 * null);
+
+console.log("5" - 1);
+
+console.log("5" + 1);
+
+console.log("vijf" * 2);
+
+console.log(false == 0);
+
+console.log(null || "user");
+
+console.log(2.5);
+
+console.log(5 == 6);
+
+console.log(5 == 5);
+
+console.log(true == 1);
+
+console.log(true === 1);
+
+console.log(null == 0);
+
+console.log(0 == "");
+
+console.log(5 == "5");
+
+console.log(5 === "5");
+
+const bmi = 67 / Math.pow(1.8, 2);
+console.log(bmi);
